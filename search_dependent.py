cb70c74fb2675ea27b50617a9488c11103796302
sousou1 <tamusou6953@gmail.com>
2019-08-22 22:43:10 +0900

    develop

--- search_dependent.py
+++ search_dependent.py
@@ -1,13 +1,13 @@
 import sys
+import re
 
 args = sys.argv
 len_args = len(args)
 
-# input python3-lark-parser ros-dashing-uncrustify-vendor-dbgsym
-
 
 path = 'src/package.txt'
 
+outfile_path = 'out.txt'
 with open(path) as f:
     lines = f.readlines()
 
@@ -17,14 +17,14 @@ packages_num = [(i, line) for i, line in enumerate(lines_strip) if 'Package: ' i
 
 depends_num = [(i, line) for i, line in enumerate(lines_strip) if 'Depends: ' in line]
 
-depends_nest_list = [[]]
+depends_nest_list = []
 
 
 def add_depends_nest_list(package_name, package_depend_depth):
-    if len(depends_nest_list) < package_depend_depth:
-        package_depend_depth.append([])
-        if package_name in depends_nest_list[package_depend_depth]:
-            depends_nest_list[package_depend_depth].append(package_name)
+    if len(depends_nest_list) < package_depend_depth + 1:
+        depends_nest_list.append([])
+    if package_name not in depends_nest_list[package_depend_depth]:
+        depends_nest_list[package_depend_depth].append(package_name)
 
 
 def search_depends(package_name):
@@ -36,26 +36,28 @@ def search_depends(package_name):
         return 0
     for depend in depends_num:
         if depend[0] >= package_line_num:
-            result_depends_list = [x.strip() for x in depend[1].replace('Depends: ', '')[0].split(',')]
-            return [x[0] for x in result_depends_list.split('(')]
+            result_depends_list = [x.strip() for x in depend[1].replace('Depends: ', '').split(',')]
+            return [re.split('[: (]', x)[0] for x in result_depends_list]
 
 
 def recursive_search(package_name):
     depends = search_depends(package_name)
     package_depend_depth = 0
-    for depend in depends:
-        depth = recursive_search(depend)
-        if depth >= package_depend_depth:
-            package_depend_depth = depth + 1
+    if depends != 0:
+        for depend in depends:
+            depth = recursive_search(depend)
+            if depth >= package_depend_depth:
+                package_depend_depth = depth + 1
     add_depends_nest_list(package_name, package_depend_depth)
     return package_depend_depth
 
 
-for i in range(len_args):
-    recursive_search(args[i])
+for i in range(len_args - 1):
+    recursive_search(args[i + 1])
 
-
-print("search python3-colcon-common-extensions")
-x = search_depends('python3-colcon-common-extensions')
-print(x)
-# packageに対応したdependsを取得
+len_depends_nest_list = len(depends_nest_list)
+for i in range(len(depends_nest_list)):
+    print(i)
+    for tmp in depends_nest_list[i]:
+        print(tmp, end=" ")
+    print("\n")
